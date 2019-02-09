9fa10dd2c1e0b31b0d8b2b02495141337aa0007f
Eddie Chu <contact@eddiechu.me>
2019-02-09 01:56:04 -0800

    debug tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const {app, runServer, closeServer} = require('../server.js');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('Server-up', function () {
+    // before(function () {
+    //     return runServer();
+    // });
+
+    // after(function () {
+    //     return closeServer();
+    // });
+
     it('should return status 200', function () {
         return chai.request(app)
             .get('/')
