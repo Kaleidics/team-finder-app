e19dd6f53a926eb45bf7bc89af9e423a840a909e
Eddie Chu <contact@eddiechu.me>
2019-02-17 12:38:07 -0800

    update auth

--- auth/router.js
+++ auth/router.js
@@ -3,19 +3,21 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
 const config = require('./config/config');
 const router = express.Router();
 
 const createAuthToken = function(user) {
     return jwt.sign({user}, config.JWT_SECRET, {
-        subject: user.username,
+        subject: user.userName,
         expiresIn: config.JWT_EXPIRY,
         algorithm: 'HS256'
     });
 }
 
 const localAuth = passport.authenticate('local', {session: false});
+
 
 
 module.exports = {router};
